0c9f1941103203f81f162b2dffc50fff94a374a2
Biktor <biktor@biktorgj.com>
2023-10-22 12:50:05 +0200

    Remove the chown and just run everything through sudo so it doesn't complain

--- modem_distro_builder/sync_and_build.sh
+++ modem_distro_builder/sync_and_build.sh
@@ -7,11 +7,11 @@ TARGET_BRANCH="mickledore"
 if [ ! -d ${TARGET_PATH}/docs ]
 then
     echo "--> Fetching the main repo..."
-    git clone -b ${TARGET_BRANCH} https://github.com/the-modem-distro/pinephone_modem_sdk ${TARGET_PATH} 
+    sudo -u buildusr git clone -b ${TARGET_BRANCH} https://github.com/the-modem-distro/pinephone_modem_sdk ${TARGET_PATH} 
 else
     echo "--> Updating the main repo..."
     cd ${TARGET_PATH} && \
-    git pull && \
+    sudo -u buildusr git pull && \
     cd $BASE_PATH
 fi
-cd ${TARGET_PATH} && ./init.sh && chown -R buildusr:buildusr ${TARGET_PATH} && sudo -u buildusr make build 
+cd ${TARGET_PATH} && sudo -u buildusr ./init.sh && sudo -u buildusr make build 
